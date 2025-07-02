71019dd136fd60782831e0a297bf15fdc41abda5
juanCristian08 <juanborba033@gmail.com>
2025-07-02 11:43:02 -0300

    migrations

--- src/database/migrations/1751467003815-CreateProduto.ts
+++ src/database/migrations/1751467003815-CreateProduto.ts
@@ -1,8 +1,43 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateProduto1751467003815 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'produto',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment'
+                    },
+                    {
+                        name: 'nome',
+                        type: 'varchar',
+                        length: '255'
+                    },
+                    {
+                        name: 'preco',
+                        type: 'decimal',
+                        precision: 10,
+                        scale: 2
+                    },
+                    {
+                        name: 'descricao',
+                        type: 'text',
+                        isNullable: true
+                    },
+                    {
+                        name: 'ativo',
+                        type: 'boolean',
+                        default: true
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
