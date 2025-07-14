38c58662258f11943d814394098a420d2efe3030
juanCristian08 <juanborba033@gmail.com>
2025-07-14 08:58:37 -0300

    atualizando api

--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -5,9 +5,9 @@ import { authJwt } from "../middlewares/auth.jwt";
 const routes = Router();
 
 routes.get("/",authJwt, UsuariosController.listar);
-routes.get("/:id", authJwt,UsuariosController.buscar)
+routes.get("/:id", UsuariosController.buscar)
 routes.post("/", UsuariosController.adicionar);
-routes.put("/:id", authJwt,UsuariosController.editar);
-routes.delete("/:id", authJwt,UsuariosController.deletar);
+routes.put("/:id", UsuariosController.editar);
+routes.delete("/:id", UsuariosController.deletar);
 
 export default routes;
